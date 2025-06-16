6a408dd511b7712f0d151de933c82c101d0bcf41
Cristina Martin Perez <cristina.martin@ciemat.es>
2025-06-16 14:29:04 +0200

    Initial commit

---
+++ scripts/2022postEE_GluGluHToEMu_cmssw-el8.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+# Run in singularity cmssw-el8
+
+# Exit on any error
+set -e
+
+# Settings
+#nevents=1000000
+fragmentfile=https://cernbox.cern.ch/s/P97JdV96JKRoaN4/download
+pileupfile="dbs:/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer22_124X_mcRun3_2022_realistic_v11-v2/PREMIX"
+#pileupfile="dbs:/MinBias_TuneCP5_13p6TeV-pythia8/Run3Summer22GS-124X_mcRun3_2022_realistic_v10-v1/GEN-SIM"
+
+# Initialize CMS VOMS proxy
+echo "==> Initializing CMS VOMS proxy..."
+voms-proxy-init --voms cms
+echo "Done."
+
+# Create and move into the working directory
+echo "==> Creating and moving into working directory 2022postEE_GluGluHToEMu..."
+mkdir -p 2022postEE_GluGluHToEMu
+cd 2022postEE_GluGluHToEMu || exit
+echo "Done."
+
+# Download config files
+echo "==> Downloading config files..."
+curl -L -o configs_2022postEE_GluGluHToEMu.zip https://cernbox.cern.ch/s/2N7s5Gn8IO4uHDI/download
+unzip configs_2022postEE_GluGluHToEMu.zip -d .
+mv 2022postEE_GluGluHToEMu/ configs/
+echo "Done."
+
+# Setup CMSSW environment
+echo "==> Setting up CMSSW_12_4_11_patch3..."
+cmsrel CMSSW_12_4_11_patch3
+cd CMSSW_12_4_11_patch3/src
+cmsenv
+echo "Done."
+
+# Download fragment
+echo "==> Downloading fragment..."
+curl -s -k --retry 3 --create-dirs -o Configuration/GenProduction/python/Run3Summer22EELHEGS-ggHiggsToEMu-fragment.py $fragmentfile
+echo "Done."
+
+# Build CMSSW
+echo "==> Building CMSSW project..."
+scram b -j $(nproc)
+cmsenv
+echo "Done."
+
+#########################
+######### LHEGS #########
+#########################
+
+# Create configuration
+#echo "==> Creating LHEGS configuration file..."
+cmsDriver.py Configuration/GenProduction/python/Run3Summer22EELHEGS-ggHiggsToEMu-fragment.py --eventcontent RAWSIM,LHE --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM,LHE --conditions 124X_mcRun3_2022_realistic_postEE_v1 --beamspot Realistic25ns13p6TeVEarly2022Collision --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="12345" --step LHE,GEN,SIM --geometry DB:Extended --era Run3 --python_filename Run3Summer22EEwmLHEGS-ggHiggsToEMu_cfg.py --fileout file:Run3Summer22EEwmLHEGS-ggHiggsToEMu.root --number $nevents --number_out $nevents --no_exec --mc
+#echo "Done."
+
+# Run production
+echo "==> Running LHEGS production..."
+#cmsRun ../../configs/Run3Summer22EEwmLHEGS-ggHiggsToEMu_cfg.py
+echo "Done."
+
+##########################
+######### PREMIX #########
+##########################
+
+# Create configuration
+#echo "==> Creating PREMIX (DIGI) configuration file..."
+cmsDriver.py  --eventcontent RAWSIM --pileup AVE_70_BX_25ns --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions 124X_mcRun3_2022_realistic_postEE_v1 --step DIGI,L1,DIGI2RAW,HLT:2022v14 --geometry DB:Extended --era Run3 --python_filename Run3Summer22EEDR-ggHiggsToEMu_1_cfg.py --fileout file:Run3Summer22EEDR-ggHiggsToEMu_0.root --filein file:Run3Summer22EEwmLHEGS-ggHiggsToEMu.root --number $nevents --number_out $nevents --pileup_input $pileupfile --no_exec --mc
+#echo "Done."
+
+# Run production
+echo "==> Running PREMIX (DIGI) production..."
+#cmsRun ../../configs/Run3Summer22EEDR-ggHiggsToEMu_1_cfg.py
+echo "Done."
+
+# Create configuration
+#echo "==> Creating PREMIX (RECO) configuration file..."
+cmsDriver.py  --eventcontent AODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --conditions 124X_mcRun3_2022_realistic_postEE_v1 --step RAW2DIGI,L1Reco,RECO,RECOSIM --geometry DB:Extended --era Run3 --python_filename Run3Summer22EEDR-ggHiggsToEMu_2_cfg.py --fileout file:Run3Summer22EEDR-ggHiggsToEMu.root --filein file:Run3Summer22EEDR-ggHiggsToEMu_0.root --number $nevents --number_out $nevents --no_exec --mc
+#echo "Done."
+
+# Run production
+echo "==> Running PREMIX (RECO) production..."
+#cmsRun ../../configs/Run3Summer22EEDR-ggHiggsToEMu_2_cfg.py
+echo "Done."
+
+cd ../../
+
+###########################
+######### MiniAOD #########
+###########################
+
+# Setup CMSSW environment
+echo "==> Setting up CMSSW_13_0_13..."
+cmsrel CMSSW_13_0_13
+cd CMSSW_13_0_13/src
+cmsenv
+echo "Done."
+
+# Create configuration
+#echo "==> Creating MiniAOD configuration file..."
+cmsDriver.py  --eventcontent MINIAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --conditions 130X_mcRun3_2022_realistic_postEE_v6 --step PAT --geometry DB:Extended --era Run3,run3_miniAOD_12X --python_filename Run3Summer22EEMiniAODv4-ggHiggsToEMu_cfg.py --fileout file:Run3Summer22EEMiniAODv4-ggHiggsToEMu.root --filein file:../../CMSSW_12_4_11_patch3/src/Run3Summer22EEDR-ggHiggsToEMu.root --number $nevents --number_out $nevents --no_exec --mc
+#echo "Done."
+
+# Run production
+echo "==> Running MiniAOD production..."
+#cmsRun ../../configs/Run3Summer22EEMiniAODv4-ggHiggsToEMu_cfg.py
+echo "Done."
+
+###########################
+######### NanoAOD #########
+###########################
+
+# Create configuration
+#echo "==> Creating NanoAOD configuration file..."
+cmsDriver.py  --eventcontent NANOEDMAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier NANOAODSIM --conditions 130X_mcRun3_2022_realistic_postEE_v6 --step NANO --scenario pp --era Run3 --python_filename Run3Summer22EENanoAODv12-ggHiggsToEMu_cfg.py --fileout file:Run3Summer22EENanoAODv12-ggHiggsToEMu.root --filein file:Run3Summer22EEMiniAODv4-ggHiggsToEMu.root --number $nevents --number_out $nevents --no_exec --mc
+#echo "Done."
+
+# Run production
+echo "==> Running NanoAOD production..."
+#cmsRun ../../configs/Run3Summer22EENanoAODv12-ggHiggsToEMu_cfg.py
+echo "Done."
+
+cd ../../
+
+# Move all output data
+echo "==> Moving all output files to directory data/..."
+mkdir -p data/
+mv CMSSW_*/src/Run3Summer22EE*ggHiggsToEMu*.root data/
+echo "Done."
+
---
+++ scripts/2023postBPix_GluGluHToEMu.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Settings
+#nevents=1000000
+fragmentfile=https://cernbox.cern.ch/s/P97JdV96JKRoaN4/download
+pileupfile="dbs:/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer23_130X_mcRun3_2023_realistic_v13-v1/PREMIX"
+#pileupfile="filelist:fileslist_Neutrino_E-10_gun.txt"
+
+# Initialize CMS VOMS proxy
+echo "==> Initializing CMS VOMS proxy..."
+voms-proxy-init --voms cms
+echo "Done."
+
+# Create and move into the working directory
+echo "==> Creating and moving into working directory 2023postBPix_GluGluHToEMu..."
+mkdir -p 2023postBPix_GluGluHToEMu
+cd 2023postBPix_GluGluHToEMu || exit
+echo "Done."
+
+# Download config files
+echo "==> Downloading config files..."
+curl -L -o configs_2023postBPix_GluGluHToEMu.zip https://cernbox.cern.ch/s/VTozZmTo4UbJU6N/download
+unzip configs_2023postBPix_GluGluHToEMu.zip -d .
+mv 2023postBPix_GluGluHToEMu/ configs/
+echo "Done."
+
+# Setup CMSSW environment
+echo "==> Setting up CMSSW_13_0_13..."
+cmsrel CMSSW_13_0_13
+cd CMSSW_13_0_13/src
+cmsenv
+echo "Done."
+
+# Download fragment
+echo "==> Downloading fragment..."
+curl -s -k --retry 3 --create-dirs -o Configuration/GenProduction/python/Run3Summer23BPixLHEGS-ggHiggsToEMu-fragment.py $fragmentfile
+echo "Done."
+
+# Build CMSSW
+echo "==> Building CMSSW project..."
+scram b -j $(nproc)
+cmsenv
+echo "Done."
+
+#########################
+######### LHEGS #########
+#########################
+
+# Create configuration
+#echo "==> Creating LHEGS configuration file..."
+cmsDriver.py Configuration/GenProduction/python/Run3Summer23BPixLHEGS-ggHiggsToEMu-fragment.py --eventcontent RAWSIM,LHE --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM,LHE --conditions 130X_mcRun3_2023_realistic_postBPix_v2 --beamspot Realistic25ns13p6TeVEarly2023Collision --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="12345" --step LHE,GEN,SIM --geometry DB:Extended --era Run3_2023 --python_filename Run3Summer23BPixwmLHEGS-ggHiggsToEMu_cfg.py --fileout file:Run3Summer23BPixwmLHEGS-ggHiggsToEMu.root --number $nevents --number_out $nevents --no_exec --mc
+#echo "Done."
+
+# Run production
+echo "==> Running LHEGS production..."
+#cmsRun ../../configs/Run3Summer23BPixwmLHEGS-ggHiggsToEMu_cfg.py
+echo "Done."
+
+cd ../../
+
+##########################
+######### PREMIX #########
+##########################
+
+# Setup CMSSW environment
+echo "==> Setting up CMSSW_13_0_14..."
+cmsrel CMSSW_13_0_14
+cd CMSSW_13_0_14/src
+cmsenv
+echo "Done."
+
+# Downloading pileup file
+echo "==> Downloading pileup file..."
+curl -L "https://cernbox.cern.ch/s/R7jRdModLKddXht/download" -o "fileslist_Neutrino_E-10_gun.txt"
+echo "Done."
+
+# Create configuration
+#echo "==> Creating PREMIX (DIGI) configuration file..."
+cmsDriver.py  --eventcontent PREMIXRAW --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions 130X_mcRun3_2023_realistic_postBPix_v2 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:2023v12 --procModifiers premix_stage2 --geometry DB:Extended --datamix PreMix --era Run3_2023 --python_filename Run3Summer23BPixDRPremix-ggHiggsToEMu_1_cfg.py --fileout file:Run3Summer23BPixDRPremix-ggHiggsToEMu_0.root --filein file:../../CMSSW_13_0_13/src/Run3Summer23BPixwmLHEGS-ggHiggsToEMu.root --number $nevents --number_out $nevents --pileup_input $pileupfile --no_exec --mc
+#echo "Done."
+
+# Run production
+echo "==> Running PREMIX (DIGI) production..."
+#cmsRun ../../configs/Run3Summer23BPixDRPremix-ggHiggsToEMu_1_cfg.py
+echo "Done."
+
+# Create configuration
+#echo "==> Creating PREMIX (RECO) configuration file..."
+cmsDriver.py  --eventcontent AODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --conditions 130X_mcRun3_2023_realistic_postBPix_v2 --step RAW2DIGI,L1Reco,RECO,RECOSIM --geometry DB:Extended --era Run3_2023 --python_filename Run3Summer23BPixDRPremix-ggHiggsToEMu_2_cfg.py --fileout file:Run3Summer23BPixDRPremix-ggHiggsToEMu.root --filein file:Run3Summer23BPixDRPremix-ggHiggsToEMu_0.root --number $nevents --number_out $nevents --no_exec --mc
+#echo "Done."
+
+# Run production
+echo "==> Running PREMIX (RECO) production..."
+#cmsRun ../../configs/Run3Summer23BPixDRPremix-ggHiggsToEMu_2_cfg.py
+echo "Done."
+
+###########################
+######### MiniAOD #########
+###########################
+
+# Create configuration
+echo "==> Creating MiniAOD configuration file..."
+cmsDriver.py  --eventcontent MINIAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --conditions 130X_mcRun3_2023_realistic_postBPix_v2 --step PAT --geometry DB:Extended --era Run3_2023 --python_filename Run3Summer23BPixMiniAODv4-ggHiggsToEMu_cfg.py --fileout file:Run3Summer23BPixMiniAODv4-ggHiggsToEMu.root --filein file:Run3Summer23BPixDRPremix-ggHiggsToEMu.root --number $nevents --number_out $nevents --no_exec --mc
+echo "Done."
+
+# Run production
+#echo "==> Running MiniAOD production..."
+##cmsRun ../../configs/Run3Summer23BPixMiniAODv4-ggHiggsToEMu_cfg.py
+#echo "Done."
+
+###########################
+######### NanoAOD #########
+###########################
+
+# Create configuration
+echo "==> Creating NanoAOD configuration file..."
+cmsDriver.py  --eventcontent NANOEDMAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier NANOAODSIM --conditions 130X_mcRun3_2023_realistic_postBPix_v2 --step NANO --scenario pp --era Run3_2023 --python_filename Run3Summer23BPixNanoAODv12-ggHiggsToEMu_cfg.py --fileout file:Run3Summer23BPixNanoAODv12-ggHiggsToEMu.root --filein file:Run3Summer23BPixMiniAODv4-ggHiggsToEMu.root --number $nevents --number_out $nevents --no_exec --mc
+echo "Done."
+
+# Run production
+#echo "==> Running NanoAOD production..."
+##cmsRun ../../configs/Run3Summer23BPixNanoAODv12-ggHiggsToEMu_cfg.py
+#echo "Done."
+
+cd ../../
+
+# Move all output data
+echo "==> Moving all output files to directory data/..."
+mkdir -p data/
+mv CMSSW_*/src/Run3Summer23BPix*ggHiggsToEMu*.root data/
+echo "Done."
+
