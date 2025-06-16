6f625b0e7e3d5b3145d01b74a3d1b6de112d5ada
Cristina Martin Perez <cristina.martin@ciemat.es>
2025-06-16 15:37:31 +0200

    test scripts

--- 2022postEE_GluGluHToTauMu_cmssw-el8.sh
+++ 2022postEE_GluGluHToTauMu_cmssw-el8.sh
@@ -2,11 +2,13 @@
 
 # Run in singularity cmssw-el8
 
+# Cloned from repo: git clone git@github.com:cmarper/Hlfv_prod_Run3.git
+
 # Exit on any error
 set -e
 
 # Settings
-nevents=1000000
+nevents=1
 
 # Initialize CMS VOMS proxy
 echo "==> Initializing CMS VOMS proxy..."
@@ -37,15 +39,15 @@ echo "Done."
 #########################
 
 # Create configuration
-#echo "==> Creating LHEGS configuration file..."
+echo "==> Creating LHEGS configuration file..."
 mkdir Configuration/GenProduction/python/
 cp ../../Hlfv_prod_Run3/fragments/fragment-ggHiggsTauMu.py Configuration/GenProduction/python/Run3Summer22EELHEGS-ggHiggsToTauMu-fragment.py
 cmsDriver.py Configuration/GenProduction/python/Run3Summer22EELHEGS-ggHiggsToTauMu-fragment.py --eventcontent RAWSIM,LHE --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM,LHE --conditions 124X_mcRun3_2022_realistic_postEE_v1 --beamspot Realistic25ns13p6TeVEarly2022Collision --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="12345" --step LHE,GEN,SIM --geometry DB:Extended --era Run3 --python_filename Run3Summer22EEwmLHEGS-ggHiggsToTauMu_cfg.py --fileout file:Run3Summer22EEwmLHEGS-ggHiggsToTauMu.root --number $nevents --number_out $nevents --no_exec --mc
-#echo "Done."
+echo "Done."
 
 # Run production
 echo "==> Running LHEGS production..."
-#cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer22EEwmLHEGS-ggHiggsToTauMu_cfg.py
+cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer22EEwmLHEGS-ggHiggsToTauMu_cfg.py
 echo "Done."
 
 ##########################
@@ -53,24 +55,25 @@ echo "Done."
 ##########################
 
 # Create configuration
-#echo "==> Creating PREMIX (DIGI) configuration file..."
-pileupfile="dbs:/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer22_124X_mcRun3_2022_realistic_v11-v2/PREMIX"
+echo "==> Creating PREMIX (DIGI) configuration file..."
+#pileupfile="dbs:/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer22_124X_mcRun3_2022_realistic_v11-v2/PREMIX"
+pileupfile="dbs:/MinBias_TuneCP5_13p6TeV-pythia8/Run3Summer22GS-124X_mcRun3_2022_realistic_v10-v1/GEN-SIM"
 cmsDriver.py  --eventcontent RAWSIM --pileup AVE_70_BX_25ns --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions 124X_mcRun3_2022_realistic_postEE_v1 --step DIGI,L1,DIGI2RAW,HLT:2022v14 --geometry DB:Extended --era Run3 --python_filename Run3Summer22EEDR-ggHiggsToTauMu_1_cfg.py --fileout file:Run3Summer22EEDR-ggHiggsToTauMu_0.root --filein file:Run3Summer22EEwmLHEGS-ggHiggsToTauMu.root --number $nevents --number_out $nevents --pileup_input $pileupfile --no_exec --mc
-#echo "Done."
+echo "Done."
 
 # Run production
 echo "==> Running PREMIX (DIGI) production..."
-#cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer22EEDR-ggHiggsToTauMu_1_cfg.py
+cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer22EEDR-ggHiggsToTauMu_1_cfg.py
 echo "Done."
 
 # Create configuration
-#echo "==> Creating PREMIX (RECO) configuration file..."
+echo "==> Creating PREMIX (RECO) configuration file..."
 cmsDriver.py  --eventcontent AODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --conditions 124X_mcRun3_2022_realistic_postEE_v1 --step RAW2DIGI,L1Reco,RECO,RECOSIM --geometry DB:Extended --era Run3 --python_filename Run3Summer22EEDR-ggHiggsToTauMu_2_cfg.py --fileout file:Run3Summer22EEDR-ggHiggsToTauMu.root --filein file:Run3Summer22EEDR-ggHiggsToTauMu_0.root --number $nevents --number_out $nevents --no_exec --mc
-#echo "Done."
+echo "Done."
 
 # Run production
 echo "==> Running PREMIX (RECO) production..."
-#cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer22EEDR-ggHiggsToTauMu_2_cfg.py
+cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer22EEDR-ggHiggsToTauMu_2_cfg.py
 echo "Done."
 
 cd ../../
@@ -87,13 +90,13 @@ cmsenv
 echo "Done."
 
 # Create configuration
-#echo "==> Creating MiniAOD configuration file..."
+echo "==> Creating MiniAOD configuration file..."
 cmsDriver.py  --eventcontent MINIAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --conditions 130X_mcRun3_2022_realistic_postEE_v6 --step PAT --geometry DB:Extended --era Run3,run3_miniAOD_12X --python_filename Run3Summer22EEMiniAODv4-ggHiggsToTauMu_cfg.py --fileout file:Run3Summer22EEMiniAODv4-ggHiggsToTauMu.root --filein file:../../CMSSW_12_4_11_patch3/src/Run3Summer22EEDR-ggHiggsToTauMu.root --number $nevents --number_out $nevents --no_exec --mc
-#echo "Done."
+echo "Done."
 
 # Run production
 echo "==> Running MiniAOD production..."
-#cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer22EEMiniAODv4-ggHiggsToTauMu_cfg.py
+cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer22EEMiniAODv4-ggHiggsToTauMu_cfg.py
 echo "Done."
 
 ###########################
@@ -101,13 +104,13 @@ echo "Done."
 ###########################
 
 # Create configuration
-#echo "==> Creating NanoAOD configuration file..."
+echo "==> Creating NanoAOD configuration file..."
 cmsDriver.py  --eventcontent NANOEDMAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier NANOAODSIM --conditions 130X_mcRun3_2022_realistic_postEE_v6 --step NANO --scenario pp --era Run3 --python_filename Run3Summer22EENanoAODv12-ggHiggsToTauMu_cfg.py --fileout file:Run3Summer22EENanoAODv12-ggHiggsToTauMu.root --filein file:Run3Summer22EEMiniAODv4-ggHiggsToTauMu.root --number $nevents --number_out $nevents --no_exec --mc
-#echo "Done."
+echo "Done."
 
 # Run production
 echo "==> Running NanoAOD production..."
-#cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer22EENanoAODv12-ggHiggsToTauMu_cfg.py
+cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer22EENanoAODv12-ggHiggsToTauMu_cfg.py
 echo "Done."
 
 cd ../../
--- 2023postBPix_VBFHToEMu.sh
+++ 2023postBPix_VBFHToEMu.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Cloned from repo: git clone git@github.com:cmarper/Hlfv_prod_Run3.git
+
 # Exit on any error
 set -e
 
 # Settings
-nevents=1000000
+nevents=1
 
 # Initialize CMS VOMS proxy
 echo "==> Initializing CMS VOMS proxy..."
@@ -35,15 +37,15 @@ echo "Done."
 #########################
 
 # Create configuration
-#echo "==> Creating LHEGS configuration file..."
+echo "==> Creating LHEGS configuration file..."
 mkdir Configuration/GenProduction/python/
 cp ../../Hlfv_prod_Run3/fragments/fragment-VBFHiggsEMu.py Configuration/GenProduction/python/Run3Summer23BPixLHEGS-VBFHiggsToEMu-fragment.py
 cmsDriver.py Configuration/GenProduction/python/Run3Summer23BPixLHEGS-VBFHiggsToEMu-fragment.py --eventcontent RAWSIM,LHE --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM,LHE --conditions 130X_mcRun3_2023_realistic_postBPix_v6 --beamspot Realistic25ns13p6TeVEarly2023Collision --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="12345" --step LHE,GEN,SIM --geometry DB:Extended --era Run3_2023 --python_filename Run3Summer23BPixwmLHEGS-VBFHiggsToEMu_cfg.py --fileout file:Run3Summer23BPixwmLHEGS-VBFHiggsToEMu.root --number $nevents --number_out $nevents --no_exec --mc
-#echo "Done."
+echo "Done."
 
 # Run production
 echo "==> Running LHEGS production..."
-#cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer23BPixwmLHEGS-VBFHiggsToEMu_cfg.py
+cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer23BPixwmLHEGS-VBFHiggsToEMu_cfg.py
 echo "Done."
 
 cd ../../
@@ -60,24 +62,25 @@ cmsenv
 echo "Done."
 
 # Create configuration
-#echo "==> Creating PREMIX (DIGI) configuration file..."
-pileupfile="dbs:/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer23_130X_mcRun3_2023_realistic_v13-v1/PREMIX"
+echo "==> Creating PREMIX (DIGI) configuration file..."
+#pileupfile="dbs:/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer23_130X_mcRun3_2023_realistic_v13-v1/PREMIX"
+pileupfile="filelist:../../pileup/Hlfv_prod_Run3/fileslist_Neutrino_E-10_gun.txt"
 cmsDriver.py  --eventcontent PREMIXRAW --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions 130X_mcRun3_2023_realistic_postBPix_v6 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:2023v12 --procModifiers premix_stage2 --geometry DB:Extended --datamix PreMix --era Run3_2023 --python_filename Run3Summer23BPixDRPremix-VBFHiggsToEMu_1_cfg.py --fileout file:Run3Summer23BPixDRPremix-VBFHiggsToEMu_0.root --filein file:../../CMSSW_13_0_17/src/Run3Summer23BPixwmLHEGS-VBFHiggsToEMu.root  --number $nevents --number_out $nevents --pileup_input $pileupfile --no_exec --mc
-#echo "Done."
+echo "Done."
 
 # Run production
 echo "==> Running PREMIX (DIGI) production..."
-#cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer23BPixDRPremix-VBFHiggsToEMu_1_cfg.py
+cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer23BPixDRPremix-VBFHiggsToEMu_1_cfg.py
 echo "Done."
 
 # Create configuration
-#echo "==> Creating PREMIX (RECO) configuration file..."
+echo "==> Creating PREMIX (RECO) configuration file..."
 cmsDriver.py  --eventcontent AODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --conditions 130X_mcRun3_2023_realistic_postBPix_v6 --step RAW2DIGI,L1Reco,RECO,RECOSIM --geometry DB:Extended --era Run3_2023 --python_filename Run3Summer23BPixDRPremix-VBFHiggsToEMu_2_cfg.py --fileout file:Run3Summer23BPixDRPremix-VBFHiggsToEMu.root --filein file:Run3Summer23BPixDRPremix-VBFHiggsToEMu_0.root  --number $nevents --number_out $nevents --no_exec --mc
-#echo "Done."
+echo "Done."
 
 # Run production
 echo "==> Running PREMIX (RECO) production..."
-#cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer23BPixDRPremix-VBFHiggsToEMu_2_cfg.py
+cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer23BPixDRPremix-VBFHiggsToEMu_2_cfg.py
 echo "Done."
 
 ###########################
@@ -85,13 +88,13 @@ echo "Done."
 ###########################
 
 # Create configuration
-#echo "==> Creating MiniAOD configuration file..."
+echo "==> Creating MiniAOD configuration file..."
 cmsDriver.py  --eventcontent MINIAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --conditions 130X_mcRun3_2023_realistic_postBPix_v6 --step PAT --geometry DB:Extended --era Run3_2023 --python_filename Run3Summer23BPixMiniAODv4-VBFHiggsToEMu_cfg.py --fileout file:Run3Summer23BPixMiniAODv4-VBFHiggsToEMu.root --filein file:Run3Summer23BPixDRPremix-VBFHiggsToEMu.root --number $nevents --number_out $nevents --no_exec --mc
-#echo "Done."
+echo "Done."
 
 # Run production
 echo "==> Running MiniAOD production..."
-#cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer23BPixMiniAODv4-VBFHiggsToEMu_cfg.py
+cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer23BPixMiniAODv4-VBFHiggsToEMu_cfg.py
 echo "Done."
 
 ###########################
@@ -99,13 +102,13 @@ echo "Done."
 ###########################
 
 # Create configuration
-#echo "==> Creating NanoAOD configuration file..."
+echo "==> Creating NanoAOD configuration file..."
 cmsDriver.py  --eventcontent NANOEDMAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier NANOAODSIM --conditions 130X_mcRun3_2023_realistic_postBPix_v6 --step NANO --scenario pp --era Run3_2023 --python_filename Run3Summer23BPixNanoAODv12-VBFHiggsToEMu_cfg.py --fileout file:Run3Summer23BPixNanoAODv12-VBFHiggsToEMu.root --filein file:Run3Summer23BPixMiniAODv4-VBFHiggsToEMu.root --number $nevents --number_out $nevents --no_exec --mc
-#echo "Done."
+echo "Done."
 
 # Run production
 echo "==> Running NanoAOD production..."
-#cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer23BPixNanoAODv12-VBFHiggsToEMu_cfg.py
+cmsRun ../../configs/Hlfv_prod_Run3/Run3Summer23BPixNanoAODv12-VBFHiggsToEMu_cfg.py
 echo "Done."
 
 cd ../../
